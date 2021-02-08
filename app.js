9ea846d41e6266d1200054e04995fd8ba535313a
idea7654 <idea7654@naver.com>
2021-02-08 11:25:19 +0900

    add host

--- app.js
+++ app.js
@@ -2,10 +2,11 @@ const express = require("express");
 const app = express();
 
 const port = 3000;
+const host = "0.0.0.0";
 
 app.set("port", port);
 app.use(express.static("public"));
 
-app.listen(port);
+app.listen(port, host);
 
 module.exports = app;
