0c0713ed442407eaf64b78bc75d3fdea1ec8bd6f
idea7654 <idea7654@naver.com>
2021-02-10 19:18:57 +0900

    add https

--- app.js
+++ app.js
@@ -1,12 +1,31 @@
 const express = require("express");
+const https = require("https");
+const fs = require("fs");
+const options = {
+  key: fs.readFileSync("./private.key"),
+  cert: fs.readFileSync("./certificate.crt"),
+};
 const app = express();
 
-const port = 3000;
+const port = 443;
 const host = "0.0.0.0";
 
 app.set("port", port);
 app.use(express.static("public"));
 
-app.listen(port, host);
+options ? https.createServer(options, app).listen(port) : undefined;
+
+options
+  ? http
+      .createServer(function (req, res) {
+        res.writeHead(301, {
+          Location: "https://" + req.headers["host"] + req.url,
+        });
+        res.end();
+      })
+      .listen(80)
+  : http.createServer(app).listen(PORT, () => {
+      console.log(`Server is running at port ${PORT}`);
+    });
 
 module.exports = app;
