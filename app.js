100079340d84a6d28000daffb77f5808eaab3fbe
idea7654 <idea7654@naver.com>
2021-05-08 21:57:57 +0900

    update https

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const https = require("https");
 const fs = require("fs");
 const options = {
-  ca: false.readFileSync("./ca_bundle.crt"),
+  ca: fs.readFileSync("./ca_bundle.crt"),
   key: fs.readFileSync("./private.key"),
   cert: fs.readFileSync("./certificate.crt"),
 };
